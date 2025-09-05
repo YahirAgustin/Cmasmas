c163e32330d7e3ef6e9afcb4667d4920adb14edc
YahirAgustin <157630965+YahirAgustin@users.noreply.github.com>
2025-09-04 21:03:10 -0600

    Practicar Declarar Arreglos

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int a[10]; //Declarar arreglo
+   a[2] = 34;
+
+   char b[20] = {'A','B','C','D'};//Llenar Arreglo
+
+   cout<<b[2]<<endl;
+   cout<<a[2];
+
+    return 0;
+}
